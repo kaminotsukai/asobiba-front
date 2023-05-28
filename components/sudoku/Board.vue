94f8c77f8c43952623286ed16aa0b4a8a6cab2f6
kaminotsukai <makoto.kami@yappli.co.jp>
2023-05-28 16:29:08 +0900

    feat: 問題追加

--- components/sudoku/Board.vue
+++ components/sudoku/Board.vue
@@ -3,7 +3,7 @@
         <div class="w-full">
             <div class="flex justify-center w-full items-center">
                 <div class="grid grid-cols-9 grid-flow-col">
-                    <div v-for="col, i1 in data.board" :key="i1">
+                    <div v-for="col, i1 in board" :key="i1">
                         <div v-for="cell, i2 in col" :key="i2">
                             <SudokuCell
                                 :value="cell.value"
@@ -18,7 +18,7 @@
                 </div>
             </div>
         </div>
-        <div class="grid grid-cols-9 gap-1 mt-1">
+        <div class="grid grid-cols-9 gap-1 mt-5">
             <div
                 v-for="value in 9"
                 :key="value"
@@ -31,118 +31,28 @@
 
 <script lang="ts">
 import { defineComponent, reactive } from "@nuxtjs/composition-api";
+import { PropType } from "vue/types/v3-component-props";
 import SudokuCell from "./Cell.vue"
+
+type Cell = {
+    value: number;
+    readonly: boolean;
+};
 
 export default defineComponent({
     name: "SudokuBoard",
     components: {
         SudokuCell,
     },
-    setup() {
+    props: {
+        board: {
+            type: Array as PropType<Cell[][]>,
+            required: true
+        }
+    },
+    setup(props) {
         let data = reactive({
             selectedCell: [-1, -1],
-            board: [
-                [
-                    { value: 1, readonly: true },
-                    { value: 2, readonly: true },
-                    { value: 3, readonly: true },
-                    { value: 4, readonly: true },
-                    { value: 5, readonly: true },
-                    { value: 6, readonly: true },
-                    { value: 7, readonly: true },
-                    { value: 8, readonly: true },
-                    { value: 9, readonly: true },
-                ],
-                [
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                ],
-                [
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                ],
-                [
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                ],
-                [
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                ],
-                [
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                ],
-                [
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                ],
-                [
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                ],
-                [
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                    { value: 0, readonly: false },
-                ],
-            ],
-            message: "aaaaaaaa"
         })
 
         const selectCell = (row: number, col: number) => {
@@ -153,11 +63,11 @@ export default defineComponent({
             if (data.selectedCell == undefined || data.selectedCell[0] == -1 || data.selectedCell[1] == -1) {
                 return
             }
-            const cell = data.board[data.selectedCell[1]][data.selectedCell[0]]
+            const cell = props.board[data.selectedCell[1]][data.selectedCell[0]]
             if (cell.readonly) {
                 return
             }
-            data.board[data.selectedCell[1]][data.selectedCell[0]].value = value
+            props.board[data.selectedCell[1]][data.selectedCell[0]].value = value
         }
 
         return {
--- pages/sudoku/index.vue
+++ pages/sudoku/index.vue
@@ -1,17 +1,100 @@
 <template>
     <div class="px-2 pt-8 sudoku-font">
-        <SudokuBoard/>
+        <SudokuBoard
+            :board="data.board"
+        ></SudokuBoard>
+        <div class="grid grid-cols-4 gap-1 mt-10">
+            <div
+                class="h-16 border rounded border-slate-200 font-display w-full bg-white items-center justify-center text-2xl cursor-pointer flex"
+                v-for="value in 10" :key="value"
+                @click="selectQuestion(value)"
+            >
+                問題{{ value }}
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, reactive } from '@nuxtjs/composition-api'
 import SudokuBoard from '~/components/sudoku/Board.vue'
+import q1 from '~/assets/contents/questions/1.json'
+import q2 from '~/assets/contents/questions/2.json'
+import q3 from '~/assets/contents/questions/3.json'
+import q4 from '~/assets/contents/questions/4.json'
+import q5 from '~/assets/contents/questions/5.json'
+import q6 from '~/assets/contents/questions/6.json'
+import q7 from '~/assets/contents/questions/7.json'
+import q8 from '~/assets/contents/questions/8.json'
+import q9 from '~/assets/contents/questions/9.json'
+import q10 from '~/assets/contents/questions/10.json'
+
+type Cell = {
+    value: number;
+    readonly: boolean;
+};
+
+type Board = Cell[][];
+
 
 export default defineComponent({
     name: "SudokuIndex",
     components: {
         SudokuBoard
+    },
+    setup() {
+        const initBoard = () => {
+            let board: Board = []
+            for (let i = 0; i < 9; i++) {
+                board[i] = [];
+                for (let j = 0; j < 9; j++) {
+                    board[i][j] = { "value": 0, "readonly": true }
+                }
+            }
+            return board
+        }
+
+        const data = reactive({
+            board: initBoard(),
+        });
+
+        const selectQuestion = async (value: number) => {
+            if (value == 1) {
+                data.board = [...q1]
+            }
+            if (value == 2) {
+                data.board = [...q2]
+            }
+            if (value == 3) {
+                data.board = [...q3]
+            }
+            if (value == 4) {
+                data.board = [...q4]
+            }
+            if (value == 5) {
+                data.board = [...q5]
+            }
+            if (value == 6) {
+                data.board = [...q6]
+            }
+            if (value == 7) {
+                data.board = [...q7]
+            }
+            if (value == 8) {
+                data.board = [...q8]
+            }
+            if (value == 9) {
+                data.board = [...q9]
+            }
+            if (value == 10) {
+                data.board = [...q10]
+            }
+        }
+
+        return {
+            data,
+            selectQuestion
+        }
     }
 })
 </script>
